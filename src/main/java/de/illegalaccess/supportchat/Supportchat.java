ccb463ce338e1edd4b7dd13e118811ccba2dd02f
IllegalAccess <felix.schick@zyonic.de>
2022-05-27 19:24:46 +0200

    maybe the projekt is finished v 3.                                 1415926535 8979323846 2643383279 5028841971 6939937510 5820974944 5923078164 0628620899 8628034825 3421170679 8214808651 3282306647 0938446095 5058223172 5359408128 4811174502 8410270193 8521105559 6446229489 5493038196 4428810975 6659334461 2847564823 3786783165 2712019091 4564856692 3460348610 4543266482 1339360726 0249141273 7245870066 0631558817 4881520920 9628292540 9171536436 7892590360 0113305305 4882046652 1384146951 9415116094 3305727036 5759591953 0921861173 8193261179 3105118548 0744623799 6274956735 1885752724 8912279381 8301194912

--- src/main/java/de/illegalaccess/supportchat/Supportchat.java
+++ src/main/java/de/illegalaccess/supportchat/Supportchat.java
@@ -2,6 +2,8 @@ package de.illegalaccess.supportchat;
 
 import de.illegalaccess.supportchat.commands.SupportCommand;
 import de.illegalaccess.supportchat.listeners.PlayerChatListener;
+import de.illegalaccess.supportchat.listeners.PlayerLoginListener;
+import de.illegalaccess.supportchat.listeners.PlayerQuitListener;
 import de.illegalaccess.supportchat.mysql.MySQL;
 import jdk.nashorn.internal.objects.annotations.Getter;
 import net.md_5.bungee.api.ProxyServer;
@@ -41,6 +43,8 @@ public final class Supportchat extends Plugin {
 
         ProxyServer.getInstance().getPluginManager().registerCommand(this, new SupportCommand());
         ProxyServer.getInstance().getPluginManager().registerListener(this, new PlayerChatListener());
+        ProxyServer.getInstance().getPluginManager().registerListener(this, new PlayerLoginListener());
+        ProxyServer.getInstance().getPluginManager().registerListener(this, new PlayerQuitListener());
 
     }
 
