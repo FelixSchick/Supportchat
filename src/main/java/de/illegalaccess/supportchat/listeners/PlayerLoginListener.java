fe241b383c1c57b2000887e80ffd4beca7190a46
IllegalAccess <felix.schick@zyonic.de>
2022-05-29 18:09:26 +0200

    fixed supporter insert bug

--- src/main/java/de/illegalaccess/supportchat/listeners/PlayerLoginListener.java
+++ src/main/java/de/illegalaccess/supportchat/listeners/PlayerLoginListener.java
@@ -2,6 +2,7 @@ package de.illegalaccess.supportchat.listeners;
 
 import de.illegalaccess.supportchat.mysql.SupporterManager;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.LoginEvent;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
